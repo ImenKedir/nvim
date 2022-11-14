acc4c595ed3cc0e6e2f3369d9b06fdb82539226b
AnaxGotJuice <64612032+ImenKedir@users.noreply.github.com>
2022-11-14 13:18:00 -0800

    initial commit

---
+++ lua/imen/plugins/lualine.lua
@@ -0,0 +1,15 @@
+-- import lualine plugin safely
+local status, lualine = pcall(require, "lualine")
+if not status then
+	return
+end
+
+-- get lualine nightfly theme
+local lualine_theme = require("lualine.themes.horizon")
+
+-- configure lualine with modified theme
+lualine.setup({
+	options = {
+		theme = lualine_theme,
+	},
+})
